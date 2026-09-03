02e37f50d5f02f3ea8991542c3fb072a53a30388
Boris Prilyan <bor.prilyan@gmail.com>
2026-09-03 22:50:16 +0700

    1.15a Contact updated

--- src/config/contact.js
+++ src/config/contact.js
@@ -1,9 +1,9 @@
 // CONTACT_DETAILS_CHANGE_HERE
 export const contactConfig = {
-  email: 'YOUR_EMAIL@agungsedayu.com',
-  whatsappDisplay: '+62 XXX-XXXX-XXXX',
+  email: 'boris.sidabutar@agungsedayu.com',
+  whatsappDisplay: '+62 881 1623 122',
 
   // Use country code and digits only when replacing this value.
   // Example: 6281234567890
-  whatsappNumber: '628XXXXXXXXXX',
+  whatsappNumber: '628811623122',
 }
